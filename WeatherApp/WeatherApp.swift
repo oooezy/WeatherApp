f804401e185a3a65658313b706661209616e41a6
eunjileee <leeeeunz@gmail.com>
2022-07-07 11:51:15 +0900

    Initialize project

---
+++ WeatherApp/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  WeatherApp
+//
+//  Created by 이은지 on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
